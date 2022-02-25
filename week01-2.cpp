7d678529521cdc2a52f5eadd37cf7f737be450c4
yanghanze1 <2452788682@qq.com>
2022-02-25 12:23:29 +0800

    add week01

---
+++ week01-2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    printf("ziyuan,zichuan:");
+
+    char c;
+    scanf("%c",&c);
+    printf("ziyuan: %c\n",c);
+
+    char line[1000];
+    scanf("%s",line);
+    printf("zichuan: %s\n",line);
+
+    return 0;
+}
---
+++ week01-3.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+char line[1000];
+int main()
+{
+    scanf("%s",line);
+    for(int i = 0; i < 1000 ; i++){
+        if(line[i] == '2' ) printf("zhaodao2\n");
+    }
+}
